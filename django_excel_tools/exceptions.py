d451a329fa2e338b9e6adf8684828fdb6077ff1e
Khath Khemanorak <khemanorak.k@codium.co>
2017-08-27 23:42:06 +0700

    Add exception

---
+++ django_excel_tools/exceptions.py
@@ -0,0 +1,17 @@
+class BaseExcelError(Exception):
+
+    def __init__(self, message):
+        super(BaseExcelError, self).__init__()
+        self.message = message
+
+
+class ValidationError(BaseExcelError):
+    pass
+
+
+class ColumnNotEqualError(BaseExcelError):
+    pass
+
+
+class FieldNotExist(BaseExcelError):
+    pass
