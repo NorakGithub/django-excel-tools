1fe7f4f2249a59b26fdbb1508f2a0d63d048d858
Khath Khemanorak <khemanorak.k@codium.co>
2017-08-28 00:03:14 +0700

    Update setup.py for distributing

--- setup.py
+++ setup.py
@@ -11,34 +11,19 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-    # TODO: put package requirements here
-]
-
-setup_requirements = [
-    'pytest-runner',
-    # TODO(NorakGithub): put setup requirements (distutils extensions, etc.) here
-]
-
-test_requirements = [
-    'pytest',
-    # TODO: put package test requirements here
-]
-
 setup(
-    name='python_excel_tools',
-    version='0.1.0',
+    name='django_excel_tools',
+    version='0.0.1',
     description="Common function when working with excel.",
     long_description=readme + '\n\n' + history,
     author="Khemanorak Khath",
     author_email='khath.khemanorak@gmail.com',
-    url='https://github.com/NorakGithub/python_excel_tools',
-    packages=find_packages(include=['python_excel_tools']),
+    url='https://github.com/NorakGithub/django_excel_tools',
+    packages=find_packages(include=['django_excel_tools']),
     include_package_data=True,
-    install_requires=requirements,
     license="MIT license",
     zip_safe=False,
-    keywords='python_excel_tools',
+    keywords='django, excel, tools',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
@@ -51,8 +36,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-    ],
-    test_suite='tests',
-    tests_require=test_requirements,
-    setup_requires=setup_requirements,
+    ]
 )
