2c3b9311db07b6960d3c1872e12880b742d22d5f
Khath Khemanorak <khemanorak.k@codium.co>
2018-01-22 19:30:47 +0700

    Update base field code for refactoring

--- django_excel_tools/fields.py
+++ django_excel_tools/fields.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+
+import datetime
+
 from .exceptions import ValidationError, SerializerConfigError
 
 BASE_MESSAGE = u'[Row {index}] {verbose_name} {message}'
@@ -19,17 +23,37 @@ class BaseField(object):
         self.cleaned_value = None
 
     def validate(self, index):
-        if not self.blank and self.value is None:
+        validating_value = self.strip_value_space()
+        validating_value = self.validate_blank(validating_value, index)
+        validating_value = self.validate_specific_data_type(validating_value, index)
+        validating_value = self.validate_default(validating_value)
+        self.cleaned_value = validating_value
+
+    def validate_blank(self, validating_value, index):
+        blank_values = ['', None]
+        if not self.blank and validating_value in blank_values:
             raise ValidationError(message=BASE_MESSAGE.format(
                 index=index,
                 verbose_name=self.verbose_name,
-                message='is not allow to be blank.')
-            )
+                message='is not allow to be blank.'
+            ))
+        return validating_value
 
-        if self.value is not None:
-            self.data_type_validate(index)
-        elif self.default is not None:
-            self.cleaned_value = self.default
+    def strip_value_space(self):
+        str_types = [str]
+        if sys.version_info < (3, 0):
+            str_types.append(unicode)
+        if type(self.value) in str_types:
+            return self.value.strip()
+        return self.value
+
+    def validate_specific_data_type(self, validating_value, index):
+        pass
+
+    def validate_default(self, validating_value):
+        if self.default is not None and validating_value is None:
+            return self.default
+        return validating_value
 
     def data_type_validate(self, index):
         if type(self.value) is str:
@@ -78,3 +102,19 @@ class BaseDateTimeField(BaseField):
         super(BaseDateTimeField, self).__init__(verbose_name, blank)
         self.date_format = date_format
         self.date_format_verbose = date_format_verbose
+
+    def convert_datetime(self, validating_value, index):
+        try:
+            return datetime.datetime.strptime(validating_value, self.date_format)
+        except ValueError:
+            raise ValidationError(message=BASE_MESSAGE.format(
+                index=index,
+                verbose_name=self.verbose_name,
+                message='"{}" is incorrect format, it should be "{}".'.format(validating_value, self.date_format)
+            ))
+
+    @staticmethod
+    def convert_int_to_str(validating_value):
+        if type(validating_value) is int:
+            str_type = str if sys.version_info >= (3, 0) else unicode
+            return str_type(validating_value)
