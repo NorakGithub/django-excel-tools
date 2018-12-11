f08fc3a16d609783d9baeb523f98d901eac347f5
Khemanorak Khath <khemanorak.k@codium.co>
2018-12-11 23:54:58 +0700

    Fixing github url and fixing pypi not format markdown correctly

--- setup.py
+++ setup.py
@@ -12,19 +12,22 @@ with open('CHANGELOG.md') as changelog_file:
     changelog = changelog_file.read()
 
 setup(
-    name='django_excel_tools',
-    version_format='{tag}',
-    setup_requires=['setuptools-git-version'],
-    description="Common function when working with excel.",
-    long_description=readme + '\n\n' + changelog,
     author="Khemanorak Khath",
     author_email='khath.khemanorak@gmail.com',
-    url='https://github.com/NorakGithub/django_excel_tools',
+    url='https://github.com/NorakGithub/django-excel-tools',
+    name='django_excel_tools',
+    version_format='{tag}',
+    description="Common function when working with excel.",
+    long_description=readme + '\n\n' + changelog,
+    long_description_content_type='text/markdown',
     packages=find_packages(include=['django_excel_tools']),
     include_package_data=True,
     license="MIT license",
     zip_safe=False,
     keywords='django, excel, tools',
+    setup_requires=[
+        'setuptools-git-version'
+    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
